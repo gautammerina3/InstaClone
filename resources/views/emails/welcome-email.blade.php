231f8a61cb0bcc4e0942cbb0224d5151890ce9c3
gautammmerina3 <gautammmerina3@gmail.com>
2019-11-08 11:27:31 +0545

    last commit

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+# Welcome to FaceGram
+
+You can experience viewing photos here.
+Thank you for joining us.
+Have a great experience here.
+
+
+All the best,<br>
+Merina
+@endcomponent
